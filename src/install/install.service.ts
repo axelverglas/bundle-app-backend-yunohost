538bc5551620a867a146d177c9c6d5a776ee8695
axel <axel@rockusupport.com>
2024-01-09 16:45:10 +0100

    add install

--- src/install/install.service.ts
+++ src/install/install.service.ts
@@ -1,40 +1,61 @@
 import { Injectable } from '@nestjs/common';
-import { promisify } from 'util';
-import { exec } from 'child_process';
+import { spawn } from 'child_process';
 
 @Injectable()
 export class InstallService {
-  private execPromise = promisify(exec);
-
   async installApps(appsData: any[]): Promise<any[]> {
     const installationResults = [];
 
     for (const appData of appsData) {
-      const { appName, mainDomain, appArgs } = appData;
-      const appDomain = `${appName}.${mainDomain}`;
+      const { appName, domain, path, password } = appData;
+
+      const command = 'ssh';
+      const args = [
+        `nom-d'utilisateurl@ip-serveur`,
+        `"sudo -S yunohost app install ${appName} --args 'domain=${domain}&path=${path}&init_main_permission=admins&password=${password}&admin=axel'"`,
+      ];
 
       try {
-        await this.execPromise(
-          `sudo ssh axel@51.158.74.176 yunohost domain add ${appDomain}`,
-        );
-        const { stdout } = await this.execPromise(
-          `sudo yunohost app install ${appName} -a "${appArgs}"`,
-        );
-
+        const result = await this.runCommand(command, args);
         installationResults.push({
           appName,
           status: 'success',
-          message: stdout,
+          message: result,
         });
       } catch (error) {
         installationResults.push({
           appName,
           status: 'failed',
-          message: error.message,
+          message: error,
         });
       }
     }
 
     return installationResults;
+  }
+
+  runCommand(command: string, args: string[]): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const process = spawn(command, args, { shell: true });
+
+      let stdout = '';
+      let stderr = '';
+
+      process.stdout.on('data', (data) => {
+        stdout += data.toString();
+      });
+
+      process.stderr.on('data', (data) => {
+        stderr += data.toString();
+      });
+
+      process.on('close', (code) => {
+        if (code === 0) {
+          resolve(stdout);
+        } else {
+          reject(stderr || `Command failed with exit code ${code}`);
+        }
+      });
+    });
   }
 }
